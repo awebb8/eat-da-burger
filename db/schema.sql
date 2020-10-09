5ed1e7e83e96f59d4c869c28e7ea058ed220ea48
Alkawe <alixkwebb@gmail.com>
2020-10-08 23:03:25 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+### Schema
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
