653d0cbf444adcbfbd532dce90f85459e1d8bbcd
Alkawe <alixkwebb@gmail.com>
2020-10-10 12:03:48 -0400

    added back objToSql function

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,30 @@ const { createPool } = require("mysql");
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+
 // Object for all our SQL statement functions.
 var orm = {
     selectAll: function(table, cb) {
@@ -29,9 +53,9 @@ var orm = {
           });
     },
 
-    updateOne: function(table, devouredCol, id, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
         
-        const queryString = "UPDATE " + table + " SET " + devouredCol + " = true WHERE " + id;
+        const queryString = "UPDATE " + table + " SET " + objToSql(objColVals) + " WHERE " + condition;
 
         console.log(queryString);
         connection.query(queryString, function(err, result) {
